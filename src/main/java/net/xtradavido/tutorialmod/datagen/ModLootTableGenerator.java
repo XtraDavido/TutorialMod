1418fbc2fb42e3f32fa4906f9db01469a6a7cd35
XtraDavido <davidokemink8@gmail.com>
2024-08-11 15:47:07 +0200

    Added Data Generators

---
+++ src/main/java/net/xtradavido/tutorialmod/datagen/ModLootTableGenerator.java
@@ -0,0 +1,17 @@
+package net.xtradavido.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModLootTableGenerator extends FabricBlockLootTableProvider {
+    protected ModLootTableGenerator(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+    }
+}
---
+++ src/main/java/net/xtradavido/tutorialmod/datagen/ModRecipeGenerator.java
@@ -0,0 +1,18 @@
+package net.xtradavido.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeGenerator extends FabricRecipeProvider {
+    public ModRecipeGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+    }
+}
