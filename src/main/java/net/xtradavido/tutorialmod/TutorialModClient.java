85b2c9ad55211d31601f6aa6306f5ddbc3d5972a
XtraDavido <davidokemink8@gmail.com>
2024-08-11 13:54:41 +0200

    Initial commit

---
+++ src/main/java/net/xtradavido/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.xtradavido.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
